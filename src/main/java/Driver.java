ea1b79c024e253ece7f45107878bb739f9ab507e
Joel Giannelli <jgiannelli13@gmail.com>
2022-06-06 13:15:41 -0700

    Basic connection to MongoDB on localhost

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,6 +1,6 @@
 import com.mongodb.*;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
+//import com.mongodb.client.MongoClients;
+//import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
@@ -19,7 +19,17 @@ import static com.mongodb.client.model.Updates.*;
 
 public class Driver {
     public static void main(String[] args) {
+        MongoClient mongo = new MongoClient("localhost", 27017);
 
+        // Creating Credentials
+        MongoCredential credential;
+        credential = MongoCredential.createCredential("sampleUser", "myDb",
+                "password".toCharArray());
+        System.out.println("Connected to the database successfully");
+
+        // Accessing the database
+        MongoDatabase database = mongo.getDatabase("myDb");
+        System.out.println("Credentials ::"+ credential);
 
 
 
