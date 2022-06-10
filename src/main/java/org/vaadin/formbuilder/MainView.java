2aff1ce7d23006ef8056a06f024b56da53f23a20
Joel Giannelli <jgiannelli13@gmail.com>
2022-06-10 07:40:56 -0700

    .gitignore

--- src/main/java/org/vaadin/formbuilder/MainView.java
+++ src/main/java/org/vaadin/formbuilder/MainView.java
@@ -11,8 +11,10 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
 
 @Route("")
+@PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
 public class MainView extends VerticalLayout {
     public MainView() {
         VerticalLayout todosList = new VerticalLayout();
