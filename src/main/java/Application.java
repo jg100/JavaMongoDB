47fa3f94dd96fe576117e9140c99bbc9de19f2af
Joel Giannelli <jgiannelli13@gmail.com>
2022-06-10 18:46:41 -0700

    Vaadin Flow implementation

---
+++ src/main/java/Application.java
@@ -0,0 +1,19 @@
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@Theme(value = "mytodo")
+@PWA(name = "My Todo", shortName = "My Todo", offlineResources = {})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
