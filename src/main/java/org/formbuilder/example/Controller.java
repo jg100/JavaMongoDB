18a9d67ca549fb467b90b412044aac5ebff5f83c
Joel Giannelli <jgiannelli13@gmail.com>
2022-06-13 21:35:22 -0700

    Binding Request params to GET function

--- src/main/java/org/formbuilder/example/Controller.java
+++ src/main/java/org/formbuilder/example/Controller.java
@@ -7,14 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Controller {
-    private static final String temp = "Hello!";
+    private static final String temp = "Hello! %s";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/form")
-    public FormEntry formentry(@RequestParam()) {
+    @GetMapping("/form") //HTTP GET req mapped to function same as @RequestMapping(method=GET)
+    public FormEntry formentry(@RequestParam(value = "name", defaultValue = "Joe") String name,
+                               @RequestParam(value = "phone", defaultValue = "000") String phone,
+                               @RequestParam(value = "email", defaultValue = "0@gmail") String email,
+                               @RequestParam(value = "refSource", defaultValue = "None") String refSource),
+                               @RequestParam(value = "insurance", defaultValue = "UBH") String insurance) {
 
+            return new FormEntry(counter.incrementAndGet(), name, phone, email,refSource,insurance);
 
     }
+
 
 
 
