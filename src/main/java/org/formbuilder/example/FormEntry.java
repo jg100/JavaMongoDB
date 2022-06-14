130950fa7200378474651c647cfdff4b6b7b7c13
Joel Giannelli <jgiannelli13@gmail.com>
2022-06-13 21:15:02 -0700

    Created form object

---
+++ src/main/java/org/formbuilder/example/FormEntry.java
@@ -0,0 +1,57 @@
+package org.formbuilder.example;
+
+public class FormEntry {
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+    private String refSource;
+    private String insurance;
+
+    public FormEntry(String fullName, String phoneNumber, String email, String refSource, String insurance) {
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.refSource = refSource;
+        this.insurance = insurance;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRefSource() {
+        return refSource;
+    }
+
+    public String getInsurance() {
+        return insurance;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRefSource(String refSource) {
+        this.refSource = refSource;
+    }
+
+    public void setInsurance(String insurance) {
+        this.insurance = insurance;
+    }
+}
