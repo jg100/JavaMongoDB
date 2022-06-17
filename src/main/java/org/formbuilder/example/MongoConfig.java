882ff6015c16a7d15ce8adf4096497c624235ff6
Joel Giannelli <jgiannelli13@gmail.com>
2022-06-16 23:59:06 -0700

    Creating MongoClient Settings

--- src/main/java/org/formbuilder/example/MongoConfig.java
+++ src/main/java/org/formbuilder/example/MongoConfig.java
@@ -1,5 +1,9 @@
 package org.formbuilder.example;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -16,6 +20,25 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
     @Override
     protected String getDatabaseName() {
         return env.getProperty("mongo.database");
+    }
+
+    /**
+     * public com.mongodb.client.MongoClient mongoClient()
+     * Return the MongoClient instance to connect to. Annotate with Bean in case you want to expose a MongoClient instance to the ApplicationContext.
+     * Override MongoConfigurationSupport.mongoClientSettings() to configure connection details.
+     * @return MongoClient type from the mongo driver
+     */
+    @Override
+    public MongoClient mongoClient() {
+        ConnectionString cstring = new ConnectionString(env.getProperty("mongo.connection.string"));
+
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder().applyConnectionString(cstring).build();
+
+
+
+
+
+
     }
 
 
